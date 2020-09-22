22815b690c3c23667889486469e0e1964b16c47f
frankjorgeg <frank.jorge@gmail.com>
2020-09-22 17:58:59 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:Efectivo/ui/login_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Efectivo.com.do',
+      theme: new ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(),
+    );
+  }
+}
